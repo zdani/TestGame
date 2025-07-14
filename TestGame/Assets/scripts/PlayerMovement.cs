cd6618f519c7cbf9e61c5f9128fc32a244349328
Nick-Pasc0 <pasco.nicholas@gmail.com>
2025-07-14 03:03:32 -0400

    Added new input system and smoothed idle animation
    I realize this should have been several separate commits, but one thing lead to another and I got carried away.
    - Changed PlayerMovement script to use Unity's new input system.
    - Organized Assets folder (moved all scenes to Scenes folder and settings to Settings folder).
    - Smoothed bear animations by moving the pivot of each frame to the bottom-center of the image, re-ordering the idle animation frames, and slowing down the idle animation.

--- TestGame/Assets/scripts/PlayerMovement.cs
+++ TestGame/Assets/scripts/PlayerMovement.cs
@@ -1,56 +1,62 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementScript : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 7f;
-    private Rigidbody2D body;
-    public Animator animator;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator animator;
+    public InputAction walkAction; // InputActions let us specify keybinds in the Unity Editor and makes the code more readable
+    public InputAction jumpAction;
+
+    private Rigidbody2D body;
+    private LayerMask groundLayerMask; // This will be used to check whether the player is on the ground
+
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         body.freezeRotation = true; // Prevent tipping over
+        walkAction.Enable(); // InputActions need to be enabled to work
+        jumpAction.Enable();
+        groundLayerMask = LayerMask.GetMask("Ground"); // Ground objects in the scene need to have their layer set to "Ground"
     }
 
-    // Update is called once per frame
-    void Update()
+    // Physics calculations should be done in FixedUpdate instead of Update
+    void FixedUpdate()
     {
         Vector2 velocity = body.linearVelocity;
 
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(body.linearVelocity.y) < 0.01f)
+        if (jumpAction.IsPressed() && PlayerIsGrounded()) // Checking whether the y velocity is close to 0 allows the player to sometimes jump again at the top of their jump. It's better to check whether the player is grounded using a raycast or collision detection (see PlayerIsGrounded() method below).
         {
-            velocity.y = jumpForce; // Jump force
-        }
-        else
-        {
-            // Only apply gravity if not jumping
-            velocity.y += Physics2D.gravity.y * Time.deltaTime;
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // Adding an upward force lets Unity handle gravity and is simpler than manually setting the velocity via code every frame (as it was before)
         }
 
         // Horizontal movement
-        float moveInput = 0f;
-        if (Input.GetKey(KeyCode.RightArrow))
+        Vector2 moveInput = walkAction.ReadValue<Vector2>(); // walkAction is a Vector2 so it'll read joystick inputs correctly
+
+        if (moveInput.x > 0) // moveInput.x will be positive when moving right
         {
             // Flip scale to face right
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y);
-            moveInput = moveSpeed;
-            
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (moveInput.x < 0) // moveInput.x will be negative when moving left
         {
             // Flip scale to face left
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
-            moveInput = -moveSpeed;
         }
-        
-        velocity.x = moveInput;
 
-        animator.SetFloat("speed", Mathf.Abs(moveInput));
+        velocity.x = moveInput.x * moveSpeed; // Scale the input value by moveSpeed
+        body.linearVelocity = new Vector2(velocity.x, body.linearVelocity.y); // Set the horizontal velocity without changing the vertical velocity
 
-        body.linearVelocity = velocity;
+        animator.SetFloat("speed", Mathf.Abs(velocity.x)); // The intention would be clearer as a boolean isMoving, but this can stay for now
+    }
+    
+    bool PlayerIsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0f, groundLayerMask);
+        return hit.collider != null;
     }
 }
