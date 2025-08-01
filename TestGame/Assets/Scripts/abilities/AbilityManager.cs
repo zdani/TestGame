d6949514ee200043b02719bfce26442ec2deea59
Nick-Pasc0 <pasco.nicholas@gmail.com>
2025-08-01 12:52:30 -0400

    Imported ice shield png
    Included boulder png for later

--- TestGame/Assets/Scripts/abilities/AbilityManager.cs
+++ TestGame/Assets/Scripts/abilities/AbilityManager.cs
@@ -7,12 +7,8 @@ public class AbilityManager : MonoBehaviour
 
     public void CastFireball()
     {
-        // Get direction the player is facing
         Vector2 shootDirection = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
-
-        // Instantiate without rotation — we’ll handle that in code
         GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPoint.position, Quaternion.identity);
-
         fireball.GetComponent<FireballProjectile>().Initialize(shootDirection);
     }
 }
