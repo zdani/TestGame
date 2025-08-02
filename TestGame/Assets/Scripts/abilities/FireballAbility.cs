80a4bec54eac0e6ed5d6b9aee515e32b155fccb3
Nick-Pasc0 <pasco.nicholas@gmail.com>
2025-08-01 20:59:55 -0400

    Changed fireball audio source to player

--- TestGame/Assets/Scripts/abilities/FireballAbility.cs
+++ TestGame/Assets/Scripts/abilities/FireballAbility.cs
@@ -4,11 +4,24 @@ public class FireballAbility : MonoBehaviour
 {
     public GameObject fireballPrefab;
     public Transform fireballSpawnPoint;
+    public AudioClip fireballAudio;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void Cast()
     {
         Vector2 shootDirection = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
         GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPoint.position, Quaternion.identity);
         fireball.GetComponent<FireballProjectile>().Initialize(shootDirection);
+
+        audioSource.clip = fireballAudio;
+        audioSource.pitch = Random.Range(0.95f, 1.15f);
+        //Debug.Log("Fireball sound pitch set to: " + audioSource.pitch);
+        audioSource.Play();
     }
 }
