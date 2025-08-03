fa42f89aac9b369407bf04aa66647262109f7aa7
Nick <pasco.nicholas@gmail.com>
2025-08-03 12:00:51 -0400

    fixed part of audio bug (#188)

--- TestGame/Assets/Scripts/Menus/VolumeSlider.cs
+++ TestGame/Assets/Scripts/Menus/VolumeSlider.cs
@@ -11,11 +11,6 @@ public class VolumeSlider : MonoBehaviour
     void Start()
     {
         volumeSlider = GetComponent<Slider>();
-
-        // Load saved volume or default to 0.7
-        float savedVolume = PlayerPrefs.HasKey("MasterVolume") ? PlayerPrefs.GetFloat("MasterVolume") : defaultVolume;
-        volumeSlider.value = savedVolume;
-        SetVolume(savedVolume); // Apply volume to mixer
     }
 
     public void SetVolume(float volume)
@@ -23,6 +18,7 @@ public class VolumeSlider : MonoBehaviour
         // Convert from 0–1 slider value to decibels
         float dB = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f;
         audioMixer.SetFloat("MasterVolume", dB);
+        Debug.Log($"Volume set to {volume} (dB: {dB})");
 
         // Save the linear volume (0–1), not the dB
         PlayerPrefs.SetFloat("MasterVolume", volume);
