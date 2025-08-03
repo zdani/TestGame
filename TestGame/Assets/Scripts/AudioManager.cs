fcf44388f3e27b13906489219fca4a6d31949548
Nick <pasco.nicholas@gmail.com>
2025-08-03 10:45:09 -0400

    Fixed issue where volume decreased when opening pause menu (#186)

---
+++ TestGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public float defaultVolume = 0.7f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float savedVolume = PlayerPrefs.HasKey("MasterVolume") ? PlayerPrefs.GetFloat("MasterVolume") : defaultVolume;
+        SetVolume(savedVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        // Convert from 0–1 slider value to decibels
+        float dB = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("MasterVolume", dB);
+
+        // Save the linear volume (0–1), not the dB
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+        PlayerPrefs.Save();
+    }
+}
